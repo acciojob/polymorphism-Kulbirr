46aa5dd73c4fce1402aac717c6e0156b09c12065
Kulbir Singh Bhandari <kulbirbhandari04@gmail.com>
2023-08-09 15:29:28 +0530

    Second try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -19,9 +19,9 @@ public class Main {
     public static void main(String[] args) {
         Product p = new Product();
 
-        System.out.println("First Result " + p.product( 2, 5));
-        System.out.println("Second Result Overload(int) " + p.product(2, 5, 2));
-        System.out.println("Third Result Overload(double) " + p.product(2.0, 5.0));
+        p.product( 2, 5);
+        p.product(2, 5, 2);
+        p.product(2.0, 5.0);
     }
 
 
