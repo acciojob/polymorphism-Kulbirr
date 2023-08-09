2f95b0425dfb80495ecb3acda39b4ddfb57d7db2
Kulbir Singh Bhandari <kulbirbhandari04@gmail.com>
2023-08-09 14:59:25 +0530

    First Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,34 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        System.out.println("First Result " + p.product( 2, 5));
+        System.out.println("Second Result Overload(int) " + p.product(2, 5, 2));
+        System.out.println("Third Result Overload(double) " + p.product(2.0, 5.0));
+    }
+
+
+
+
+
+
+
 
 }
