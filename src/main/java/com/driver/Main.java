48978d44eee19279747028a9f2003c962abdc7d0
Kulbir Singh Bhandari <kulbirbhandari04@gmail.com>
2023-08-09 15:41:16 +0530

    Final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,14 +4,17 @@ public class Main {
     public static class Product{
 
         public int product(int x, int y) {
+
             return x*y;
         }
 
         public int product(int x, int y, int z){
+
             return x*y*z;
         }
 
         public double product(double x, double y){
+
             return x*y;
         }
     }
